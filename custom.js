049736f5eb437fa86389de5d9434303465d7e8fd
Sarvinoz Xushvaqova <132272733+KhushvakovaSarvinoz@users.noreply.github.com>
2023-06-11 07:15:06 -0600

    +

---
+++ custom.js
@@ -0,0 +1,40 @@
+const toDoItems = document.querySelectorAll(".to-do-items")[0];
+const input = document.querySelector("#input");
+const trashIcon = document.querySelector("#trash");
+
+input.addEventListener("keydown", function(event){
+    if(event.key === "Enter")
+    addItem();
+})
+
+function addItem(){
+    const divParent = document.createElement("div");
+    const divChild = document.createElement("div");
+    const checkIcon = document.createElement("i");
+    const trashIcon = document.createElement("i");
+
+    divParent.className = "item";
+    divParent.innerHTML = '<div>'+input.value+'</div>';
+
+    checkIcon.className = "fas fa-check-square";
+    checkIcon.style.color = "lightgrey";
+    checkIcon.addEventListener("click", function(){
+        checkIcon.style.color = "limegreen";
+    })
+
+    divChild.appendChild(checkIcon);
+
+    trashIcon.className = "fas fa-trash";
+    trashIcon.style.color = "darkgray";
+    trashIcon.addEventListener("click", function(){
+        divParent.remove();
+    })
+
+    divChild.appendChild(trashIcon);
+
+    divParent.appendChild(divChild);
+    toDoItems.appendChild(divParent);
+
+    input.value = '';
+
+}
